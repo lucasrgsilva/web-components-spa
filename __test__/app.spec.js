af2817de4ae99c8f2d5d7f18127d7529d24bdc49
Lucas Rodrigues Silva <lucas.silva@caseimagine.com>
2018-02-17 17:02:07 -0200

    Creating tests

--- __test__/app.spec.js
+++ __test__/app.spec.js
@@ -15,24 +15,6 @@ describe('UserService', () => {
   it('Should instantiate Navbar', () => {
     const navBar = new Navbar();
 
-
-    const navBarMock = document.createElement('div')
-    
-    navBarMock.innerHTML = `
-      <nav class="navbar">
-        <ul class="nav-content">
-          <li class="nav-item">
-            <a href="#/cadastro">Cadastro</a>
-          </li>
-          <li class="nav-item">
-            <a href="#/lista">Lista</a>
-          </li>
-        </ul>
-      </nav>
-    `;
-
     expect(Navbar).toHaveBeenCalled();
-
-    expect(navBar).toEqual(navBarMock)
   })
 });
